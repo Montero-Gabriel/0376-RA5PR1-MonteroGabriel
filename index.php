dbe5f837257b954432ab3a3b586552832a2c6e62
Gabriel Montero <kmontero25@ilb.cat>
2026-03-25 12:25:53 +0100

    Missatge de error

--- index.php
+++ index.php
@@ -12,3 +12,13 @@ $valid = ($numero >= 1 && $numero <= 12);
   <title>Taula de multiplicar</title>
 </head>
 <body>
+  <h1>Taula de multiplicar del <?php echo $numero; ?></h1>
+  <!--Mostrar el missatge de error-->
+  <?php if (!$valid): ?>
+    <p style="color: red; font-weight: bold;">
+      Error:El teu número no està entre el 1 i el 12.
+      El número que has posat és <strong><?php echo $numero; ?></strong>
+    </p>
+  <?php else: ?>
+</body>
+</html>
