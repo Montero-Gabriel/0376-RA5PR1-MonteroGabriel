9c5f5992120425aa537ed3f48fb30d7e3e994af8
MonteroGabriel <kmontero25@ilb.cat>
2026-03-25 11:54:55 +0100

    Num de la variable/Comrpovació del num en el rang

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+//Definir el número a multiplicar
+$numero = 7;
+
+//Comprova si el número està fora del rang permès
+$valid = ($numero >= 1 && $numero <= 12);
+?>
+<!DOCTYPE html>
+<html lang="ca">
+<head>
+  <meta charset="UTF-8" />
+  <title>Taula de multiplicar</title>
+</head>
+<body>
